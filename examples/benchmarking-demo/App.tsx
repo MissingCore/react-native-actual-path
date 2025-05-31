ef8b5f9995099a0436e13fd0654d3933f7a16697
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-05-30 20:48:50 -0400

    chore: Update example

--- examples/benchmarking-demo/App.tsx
+++ examples/benchmarking-demo/App.tsx
@@ -1,8 +1,8 @@
-import * as FileSystem from 'expo-file-system';
+import { StorageAccessFramework as SAF } from 'expo-file-system';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import type { TextProps } from 'react-native';
-import { StyleSheet, Text as RNText, View } from 'react-native';
+import { StyleSheet, Pressable, Text as RNText, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -10,35 +10,61 @@ import {
 
 import { getActualPath } from '@missingcore/react-native-actual-path';
 
-export default function RootLayer() {
+export default function Root() {
   return (
     <SafeAreaProvider>
-      <Container>
-        <App />
-      </Container>
+      <StatusBar style="dark" />
+      <App />
     </SafeAreaProvider>
   );
 }
 
-export function App() {
-  return (
-    <>
-      <Text style={styles.heading}>
-        Hello
-      </Text>
-    </>
-  );
-}
-
-function Container({ children }: { children: React.ReactNode }) {
+function App() {
   const insets = useSafeAreaInsets();
+
+  const [contentUri, setContentUri] = useState<string | null>(null);
+  const [resolvedUri, setResolvedUri] = useState<string | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const pickPath = useCallback(async () => {
+    const permissions = await SAF.requestDirectoryPermissionsAsync();
+    if (!permissions.granted) return;
+    setContentUri(permissions.directoryUri);
+    try {
+      const dirContents = await SAF.readDirectoryAsync(
+        permissions.directoryUri
+      );
+      const dirItem = dirContents[0];
+      if (!dirItem) throw new Error('No items found in directory.');
+      const resolved = await getActualPath(dirItem);
+      setResolvedUri(resolved.split('/').slice(0, -1).join('/'));
+    } catch (err) {
+      console.log(err);
+      setError(err as Error);
+    }
+  }, []);
+
   return (
     <View style={[styles.container, { paddingTop: insets.top + 64 }]}>
-      <StatusBar style="dark" />
-      {children}
+      <Pressable onPress={pickPath} style={styles.button}>
+        <Text style={{ color: 'white' }}>Pick Directory</Text>
+      </Pressable>
+      <View>
+        <Text style={styles.heading}>Content URI</Text>
+        <Text>{contentUri}</Text>
+      </View>
+      <View>
+        <Text style={styles.heading}>Resolved URI</Text>
+        <Text>{resolvedUri}</Text>
+      </View>
+      <View>
+        <Text style={styles.heading}>Error</Text>
+        <Text>{error?.message}</Text>
+      </View>
     </View>
   );
 }
+
 function Text({ style, ...props }: TextProps) {
   // eslint-disable-next-line react-native/no-inline-styles
   return <RNText style={[{ color: 'black' }, style]} {...props} />;
@@ -47,8 +73,14 @@ function Text({ style, ...props }: TextProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    gap: 8,
+    alignItems: 'center',
+    gap: 32,
     backgroundColor: '#ffffff',
+  },
+  button: {
+    padding: 16,
+    backgroundColor: '#2255e3',
+    borderRadius: 16,
   },
   heading: {
     marginHorizontal: 16,
@@ -58,27 +90,5 @@ const styles = StyleSheet.create({
   text: {
     marginHorizontal: 16,
     textAlign: 'center',
-  },
-  metadataContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 16,
-    padding: 8,
-    margin: 8,
-    borderRadius: 16,
-    backgroundColor: '#ebebeb',
-    elevation: 4,
-  },
-  image: {
-    width: 150,
-    height: 150,
-    backgroundColor: '#bdbdbd',
-    borderRadius: 12,
-  },
-  infoContainer: {
-    flex: 1,
-  },
-  bold: {
-    fontWeight: 'bold',
   },
 });
